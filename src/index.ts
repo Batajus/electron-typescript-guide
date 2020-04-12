44a4b920cb2d6739d1935311a18c866e7e0e7e88
Batajus <ott-alexander@gmx.net>
2020-04-13 00:56:18 +0200

    reduce code complexity

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
-export class ElectronProject {
-
-    private win: BrowserWindow;
-
-    constructor() {
-        this.win = new BrowserWindow({
-            width: 600,
-            height: 400
-        });
-        this.init();
-    }
-
-    private init() {
-        const indexHTML = path.join(__dirname + '/index.html');
-        this.win.loadFile(indexHTML).then(() => {
-
-            // IMPLEMENT FANCY STUFF HERE
-
-        }).catch(e => console.error(e));
-    }
-
-}
-
-
-let project: ElectronProject;
 app.on('ready', () => {
-    project = new ElectronProject();
+    console.log('App is ready');
+
+    const win = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+
+    const indexHTML = path.join(__dirname + '/index.html');
+    win.loadFile(indexHTML).then(() => {
+        // IMPLEMENT FANCY STUFF HERE
+    }).catch(e => console.error(e));
 });

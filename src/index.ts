56bae216ea54b2d498ca7c6a82c8db2cef280936
Batajus <ott-alexander@gmx.net>
2022-08-04 18:15:49 +0200

    update to latest versions + formatting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
 
-app.on('ready', () => {
-    console.log('App is ready');
+app.on("ready", () => {
+  console.log("App is ready");
 
-    const win = new BrowserWindow({
-        width: 600,
-        height: 400
-    });
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
 
-    const indexHTML = path.join(__dirname + '/index.html');
-    win.loadFile(indexHTML).then(() => {
-        // IMPLEMENT FANCY STUFF HERE
-    }).catch(e => console.error(e));
+  const indexHTML = path.join(__dirname + "/index.html");
+  win
+    .loadFile(indexHTML)
+    .then(() => {
+      // IMPLEMENT FANCY STUFF HERE
+    })
+    .catch((e) => console.error(e));
 });
